b39606c040564ed06d58848a88d86ace229af6a3
merakulus <merakulus@gmail.com>
2020-03-25 23:47:28 +1100

    add CLASS example

---
+++ device.py
@@ -0,0 +1,40 @@
+from toolbox import generate_device_name, is_ipv4_address
+
+class Interface:
+    def __init__(self, name, address):
+        self.name = name
+        self._address = address
+        self.state = "Down"
+
+    @property
+    def address(self):
+        return self.address
+
+    @address.setter
+    def address(self, value)
+        if not is_ipv4_address(value):
+            raise ValueError (f">> {value} is not a valid ipv4 address")
+        self._address = value
+
+    def __rep__(self):
+        return str(vars(self))
+
+
+class Device:
+    def __init__(self, hostname): 
+        self.hostname = hostname 
+        self.motd = None
+        self.interface = None
+
+    def show(self, p = None): 
+        if not p: 
+            return str(vars(self)) 
+        elif hasattr(self, p): 
+            return (getattr(self, p)) 
+        else: 
+            return f">> no attribute '{p}'"
+
+
+class Rourter(Device)
+    pass
+
